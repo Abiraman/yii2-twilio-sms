9b76b3933acf3b0f55755a078ba41ad2e495f92f
Abiraman Ramanathan <abiraman@studioq.co.in>
2016-07-26 11:10:56 +0530

    twilio library with extension functions

--- Twilio.php
+++ Twilio.php
@@ -1,14 +1,22 @@
 <?php
 
 namespace twilio;
-
+use twilio\service\twilio;
 /**
  * This is just an example.
  */
-class AutoloadExample extends \yii\base\Widget
+class Twilio extends \yii\base\Widget
 {
-    public function run()
-    {
-        return "Hello!";
+    public $sid;
+    public $token;
+    public $twilioNumber;
+    public static function sendSms($number,$message){
+       $service=  new twilio\Services_Twilio($this->sid,  $this->token);
+       try {
+          $message = $service->account->messages->sendMessage($this->twilioNumber, $number,$message ); 
+       } catch (Services_Twilio_RestException $e) {
+           throw $e->getMessage();
+       }
+       
     }
 }
